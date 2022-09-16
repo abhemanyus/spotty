9c7f40a49bd46c11646384b444a1823423011bca
Abhemanyu sarin <abhemanyus@gmail.com>
2022-09-16 10:18:21 +0530

    create

--- api.ts
+++ api.ts
@@ -1,14 +1,31 @@
-import { api, body, endpoint, pathParams, request, response, String, securityHeader } from "@airtasker/spot";
+import { api, body, endpoint, pathParams, request, response, String, securityHeader, config, queryParams } from "@airtasker/spot";
 
+/**
+ * My really cool API
+ * Look upon it, ye mighty, and despair
+ */
 @api({ name: "Pocket Pets API", version: "0.4.2" })
+@config({
+  paramSerializationStrategy: {
+    query: {
+      array: "comma"
+    }
+  }
+})
 class Api {
   @securityHeader
   "Authorization": string
 }
 
+/**
+ * Created a new user
+ * @example
+ * wahhahaha
+ */
 @endpoint({
   method: "POST",
-  path: "/users"
+  path: "/users",
+  tags: ["USER", "TENANT"]
 })
 class CreateUser {
   @request
@@ -24,7 +41,8 @@ class CreateUser {
 
 @endpoint({
   method: "DELETE",
-  path: "/user/:uuid"
+  path: "/user/:uuid",
+  tags: ["USER"]
 })
 class DeleteUser {
   @request
@@ -41,15 +59,63 @@ class DeleteUser {
   ) { }
 }
 
+@endpoint({
+  method: "GET",
+  path: "/user/:uuid",
+  tags: ["USER"]
+})
+class FindUser {
+  @request
+  request(
+    @pathParams
+    pathParams: {
+      uuid: Uuid
+    },
+    @queryParams
+    queryParams: {
+      /**
+       * @schemaprop example
+       * ["name", "number", "address"]
+       */
+      extend: UserFields[]
+    }
+  ) { }
+
+  @response({ status: 200 })
+  successfulResponse(
+    @body body: CreateUserResponse
+  ) { }
+}
+
+
+/**
+ * Some Interface Documentation
+ * @example
+ * {firstName: "Abhemanyu", lastName: "Sarin"}
+ */
 interface CreateUserRequest {
+  /** First name of user
+   * @default
+   * "Abhemanyu"
+   */
   firstName: String;
+  /** Last name of user */
   lastName: String;
 }
 
+
+/**
+ * Some Interface Documentation
+ */
 interface CreateUserResponse {
   firstName: String;
   lastName: String;
   role: String;
 }
 
+/**
+ * A MongoDB Object ID
+ */
 type Uuid = string;
+
+type UserFields = "One" | "Two" | "Three"
